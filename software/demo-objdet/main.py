f1fb850f78d11b734f396c2c8d3bc2e7daf45d08
Corbin Eric Jarrett <jarrec1@mcmaster.ca>
2024-10-27 15:44:03 -0400

    Add initial script and README
    This commit adds a simple python script for the object detection demo.
    The script will record a video using the default camera, applies a
    blue mask (extract only blue values in a certain range), and applies
    the mask to the video.
    
    This uses opencv and a tutorial can be found at:
    https://docs.opencv.org/3.4/df/d9d/tutorial_py_colorspaces.html
    
    Opencv and python must be installed. Instructions on how to do so are
    in the README.md file.
    
    No formal testing has been done since this is an early version of
    a test script.

---
+++ software/demo-objdet/main.py
@@ -0,0 +1,42 @@
+# Display webcam or video
+# Adds a mask to the video for a particular HSV colour
+# see https://docs.opencv.org/3.4/df/d9d/tutorial_py_colorspaces.html
+import cv2
+
+# source of video
+cap = cv2.VideoCapture(0) # laptop camera
+# cap = cv2.VideoCapture("path\to\file") # video in filesystem
+
+# read frame, calculate mask, display frame and mask
+while True:
+    ret, frame = cap.read()
+
+    # convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # ranges of colours are in (H,S,V) format
+    # cv2.inRange(hsv, lower, upper)
+    # to determine lower and upper use an HSV colour picker online
+
+    ## mask of GREEN (36,25,25) ~ (89,255,255) 
+    # mask = cv2.inRange(hsv, (36, 25, 25), (89,255,255))
+
+    ## mask of RED
+    # mask = cv2.inRange(hsv, (175,50,20), (180,255,255))
+
+    ## mask of BLUE
+    mask = cv2.inRange(hsv, (90,150,90), (135,255,255))
+
+    # display
+    cv2.imshow('Video Capture', frame)
+    cv2.imshow('mask',mask)
+    # to display mask and input video
+    #cv2.imshow('mask AND video', cv2.bitwise_and(frame,frame,mask=mask))
+
+    # if q pressed, exit loop
+    if cv2.waitKey(5) == ord('q'):
+        break
+
+# clean up
+cap.release()
+cv2.destroyAllWindows()
