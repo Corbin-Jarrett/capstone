edd704a234224c3b11fb269385cba3101a27964a
CorbinJarrett <corbin.jarrett@gmail.com>
2025-04-06 16:15:51 -0400

    BLE client changes
    Changed how the ble client connects to be more efficient and use less whiles.
    It also connects and reconnects in a separate function and waits to
    reconnect before continuing.
    
    We tested it and it was connecting consistently and staying connected.
    It had some delays but they were due to errors on the ESP side.

--- software/phase2/eyecan.py
+++ software/phase2/eyecan.py
@@ -18,7 +18,6 @@ READ_CHAR_UUID = "0000FEF4-0000-1000-8000-00805F9B34FB"
 WRITE_CHAR_UUID = "0000DEAD-0000-1000-8000-00805F9B34FB"
 # global variables
 delim = ', '
-signal = 1
 threshold_1 = 10
 threshold_2 = 5
 esp32_address = None
@@ -49,15 +48,33 @@ async def find_esp32():
             return device.address
     return None
 
-async def ble_message(client, user_distance, threshold_outer=-1):            
-        # Write message to ESP32
-        if not client.is_connected:
-            print("CLIENT IS NOT CONNECTED")
-            return
-        message = str(signal) + delim + str(user_distance) + delim + str(threshold_outer) + delim + str(threshold_2) + '\r'
-        print(f"Sending: {message}")
-        await client.write_gatt_char(WRITE_CHAR_UUID, message.encode(), response=False) # not currently handling response so will continue immediately
-        print("Message sent!")
+async def ble_message(client, signal_in, user_distance):
+    # Write message to ESP32
+    if not client.is_connected:
+        print("CLIENT IS NOT CONNECTED")
+        return
+    message = str(signal_in) + delim + str(user_distance) + delim + str(threshold_1) + delim + str(threshold_2) + '\r'
+    print(f"Sending: {message}")
+    await client.write_gatt_char(WRITE_CHAR_UUID, message.encode(), response=False) # not currently handling response so will continue immediately
+    print("Message sent!")
+
+async def connect_ble(client):
+    while True:
+        try:
+            print(f"Attempting to connect to {esp32_address}...")
+            await asyncio.wait_for(client.connect(), timeout=10)
+
+            if client.is_connected:
+                print(f"Connected to {esp32_address}")
+                return
+        except asyncio.TimeoutError:
+            print("Connection attempt timed out. Retrying in 1 second...")
+        except BleakError as e:
+            print(f"BLE connection failed: {e}. Retrying in 1 second...")
+        except Exception as e:
+            print(f"Unexpected error: {e}. Retrying in 1 second...")
+
+        await asyncio.sleep(1)  # Wait before retrying
 
 # Connect to ESP32
 esp32_address = asyncio.run(find_esp32())
@@ -73,52 +90,50 @@ p2.start()
 
 
 async def main():
+    client = BleakClient(esp32_address, timeout=10.0)
+    await connect_ble(client)
+
     while True:
         try:
-            async with BleakClient(esp32_address, timeout=10.0) as client:
-                print(f"Connected to {esp32_address}")
+            # change GUI in here
 
-                await asyncio.sleep(2)  # Add delay for ESP32 to stabilize connection
+            # check if noir and thermal are ready
+            # print(f"checking if image captured: {time.time()}")
+            local_ready = False
+            while not local_ready:
+                local_ready = (picam_ready.value == 0) and (thermal_ready.value == 0)
 
-                # main while loop
-                while client.is_connected:
-                    #try:
-                        # change GUI in here
+            # go through each hazard contour and find distance to each hand point
+            dist_array = []
+            num_hazards = thermal_data[0]
+            num_hands = hand_data[0]
+            hand_depth = 232*(hand_data[1]-0.125) # numbers come from manual calibration
+            for i in range(num_hazards):
+                contour = thermal_data[i+1]
 
-                        # check if noir and thermal are ready
-                        # print(f"checking if image captured: {time.time()}")
-                        local_ready = False
-                        while not local_ready:
-                            local_ready = (picam_ready.value == 0) and (thermal_ready.value == 0)
+                for j in range(num_hands):
+                    for point in hand_data[j+2]:
+                        dist = cv.pointPolygonTest(contour,point,True)
+                        # print(f"distance between hazard {i} and point {point}: {dist}")
+                        dist_array.append(dist)
 
-                        # go through each hazard contour and find distance to each hand point
-                        dist_array = []
-                        num_hazards = thermal_data[0]
-                        num_hands = hand_data[0]
-                        hand_depth = 232*(hand_data[1]-0.125) # numbers come from manual calibration
-                        for i in range(num_hazards):
-                            contour = thermal_data[i+1]
+            # if dist_array is empty, either no hand or hazard detected
+            if dist_array:
+                pixel_distance = max(dist_array)
+                cm_distance = scale_factor*pixel_distance
+                dist_3d = (math.sqrt((cm_distance**2) + (hand_depth**2))) - 5   # set 5 cm as height of hazard
+                print(f"closest distance: {dist_3d} cm")
+                await ble_message(client, 1, cm_distance)
+            else:
+                await ble_message(client, 0, 0)
 
-                            for j in range(num_hands):
-                                for point in hand_data[j+2]:
-                                    dist = cv.pointPolygonTest(contour,point,True)
-                                    # print(f"distance between hazard {i} and point {point}: {dist}")
-                                    dist_array.append(dist)
+            # ready for another image
+            # print(f"ready to capture image: {time.time()}")
+            picam_ready.value = 1
+            thermal_ready.value = 1
 
-                        # if dist_array is empty, either no hand or hazard detected
-                        if dist_array:
-                            pixel_distance = max(dist_array)
-                            cm_distance = scale_factor*pixel_distance
-                            dist_3d = (math.sqrt((cm_distance**2) + (hand_depth**2))) - 5   # set 5 cm as height of hazard
-                            print(f"closest distance: {dist_3d} cm")
-                            await ble_message(client, cm_distance, threshold_1)
-                        else:
-                            await ble_message(client, 0)
-
-                        # ready for another image
-                        # print(f"ready to capture image: {time.time()}")
-                        picam_ready.value = 1
-                        thermal_ready.value = 1
+            if not client.is_connected:
+                await connect_ble(client)
 
         except BleakError as e:
             print(f"Connection failed: {e}. Retrying in 1 second...")
@@ -129,6 +144,9 @@ async def main():
             await asyncio.sleep(1)  # Wait before reconnecting
 
         except KeyboardInterrupt:
+            if client.is_connected:
+                print("Disconnecting...")
+                await client.disconnect()
             return
 
 
